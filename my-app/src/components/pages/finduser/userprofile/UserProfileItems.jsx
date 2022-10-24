3803f11feb37f2423c6cb8a23d2ae1d9fa283c47
Alexandr <115074164+Fmpime@users.noreply.github.com>
2022-10-24 21:11:41 +0300

    10 ubeley
    class component

--- my-app/src/components/pages/finduser/userprofile/UserProfileItems.jsx
+++ my-app/src/components/pages/finduser/userprofile/UserProfileItems.jsx
@@ -1,19 +1,31 @@
 import React from "react";
 import UserProfileItem from "./userprofileitem/UserProfileItem";
-
-const UserProfileItems = ({findUserData,follow,unfollow}) => {
+import axios from "axios";
+import classes from './../FindUser.module.css'
+class UserProfileItems extends React.Component {
+    componentDidMount(){axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response=>{
+    this.props.setUsers(response.data.items)
+    })   
+  }
+  render(){
   return (
     <div>
-      {findUserData.map((state) => {
+      <div className={classes.User_profile_box}>
+      {this.props.findUserData.map((state) => {
         return (
-          <div>
-            <UserProfileItem  state={state} unfollow={unfollow} follow={follow}/>
-          </div>
+            <UserProfileItem
+              state={state}
+              unfollow={this.props.unfollow}
+              follow={this.props.follow}
+            />
         );
       })}
-      <button></button>
+      </div>
+      <button
+      onClick={this.setUsersFunc}
+      >JXDN</button>
     </div>
-  );
+  );}
 };
 
 export default UserProfileItems;
--- my-app/src/components/pages/finduser/userprofile/userprofileitem/UserProfileItem.jsx
+++ my-app/src/components/pages/finduser/userprofile/userprofileitem/UserProfileItem.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import classes from "./../../FindUser.module.css";
 const UserProfileItem = (props) => {
+  console.log(props.state)
     const follow=(id)=>{
         props.follow(id)
         
@@ -12,19 +13,24 @@ const UserProfileItem = (props) => {
     <div className={classes.user__profile__item}>
       <div className={classes.user_profile_info}>
         <div>
-          <img src={props.state._img} alt="qwe" />
+          <img src={
+            props.state.photos.small != null
+            ?props.state.photos.small
+            :"https://bain.design/wp-content/uploads/2014/08/People-Avatar-Set-Rectangular-12.jpg"}
+            alt="qwe" />
           <place>
-            {props.state._Country},{props.state._State}
+            Country,
+            State
           </place>
         </div>
         <div className={classes.user_profile_info_text}>
-          <h4>{props.state._NickName}</h4>
-          <p>{props.state._userStaus}</p>
+          <h4>{props.state.name}</h4>
+          <p>{props.state.status}</p>
         </div>
       </div>
-      {props.state._followStatus
-        ?<button onClick={()=>follow(props.state._id)}>follow</button>
-        :<button onClick={()=>unfollow(props.state._id)}>unfollow</button>
+      {props.state.followed
+        ?<button onClick={()=>follow(props.state.id)}>unfollow</button>
+        :<button onClick={()=>unfollow(props.state.id)}>follow</button>
         }
     </div>
   );
