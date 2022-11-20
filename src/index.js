b3d079a8b61b0fca1c73847231961b43797f6567
Alexandr <115074164+Fmpime@users.noreply.github.com>
2022-11-20 03:30:04 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import "./index.css";
 import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
   root.render(
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename="/">
       <Provider store={store}>
         <App />
       </Provider>
