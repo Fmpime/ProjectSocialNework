d41609ce868dde174702bf0b732ed46774e2446e
Alexandr <115074164+Fmpime@users.noreply.github.com>
2022-11-20 17:49:08 +0300

    Vers 18
    q

--- src/components/pages/messages/dialogs/Dialog.jsx
+++ src/components/pages/messages/dialogs/Dialog.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import DialogsItem from './dialogitem/DialogsItem';
 import classes from "../Messages.module.css";
+import { withRouter } from '../../../../hoc/WithRouerHOC';
+
+
 const Dialog = (props) => {
-
       props.updataDialogThunkCreator()
-
-      
-
     return (
         <div className={classes.dialog}>
         {props.dialogsData.map((dialog) => {
-          return <DialogsItem name={dialog.userName} id={dialog.id} />;
+          return <DialogsItem key={dialog.id} name={dialog.userName} id={dialog.id} />;
         })}
       </div>
     );
 }
 
-export default Dialog;
+export default withRouter(Dialog);
--- src/components/pages/messages/dialogs/dialogitem/DialogsItem.jsx
+++ src/components/pages/messages/dialogs/dialogitem/DialogsItem.jsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { withRouter } from '../../../../../hoc/WithRouerHOC';
 import classes from './DialogsItem.module.css'
 const DialogsItem = (props) => {
-
+    const [onDialogs, setOnDialogs] = useState(false);
+    useEffect(() => {
+        const pageId = props.router.params['*']
+        if (pageId==props.id){
+            setOnDialogs(true)
+        }else{setOnDialogs(false)}
+      }, [props.router.params]);
 
         
     
     return (
-           <Link className={classes.dialogs__items} to={`/messages/${props.id}`}><img src="https://bain.design/wp-content/uploads/2014/08/People-Avatar-Set-Rectangular-12.jpg" alt="avatar" /><div>{props.name}</div></Link>       
+           <Link className={onDialogs?classes.dialogs__items+" "+classes.dialogs__items__selected:classes.dialogs__items} to={`/messages/${props.id}`}><img src="https://bain.design/wp-content/uploads/2014/08/People-Avatar-Set-Rectangular-12.jpg" alt="avatar" /><div>{props.name}</div></Link>       
     );
 }
 
-export default DialogsItem;
+export default withRouter(DialogsItem);
