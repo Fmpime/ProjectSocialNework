552a0f2de0a5f22c77082423b08a102efff7e4b5
Alexandr <115074164+Fmpime@users.noreply.github.com>
2022-11-20 03:47:50 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import "./index.css";
 import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
   root.render(
-    <HashRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter >
       <Provider store={store}>
         <App />
       </Provider>
