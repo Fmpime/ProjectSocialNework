9ccb21c5d49cc288836b36f5954ef0964b8c9010
Alexandr <115074164+Fmpime@users.noreply.github.com>
2022-11-20 03:40:12 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import { Provider } from "react-redux";
 import store from "./redux/ReduxStore";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
   root.render(
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   );
 
 
