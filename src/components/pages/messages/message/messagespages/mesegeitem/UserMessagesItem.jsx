20b4df3db7ea53a5ccf9a04681f7650acc54ecb6
Alexandr <115074164+Fmpime@users.noreply.github.com>
2022-11-21 09:27:03 +0300

    Update UserMessagesItem.jsx

--- src/components/pages/messages/message/messagespages/mesegeitem/UserMessagesItem.jsx
+++ src/components/pages/messages/message/messagespages/mesegeitem/UserMessagesItem.jsx
@@ -10,9 +10,16 @@ const UserMessagesItem = (props) => {
           <h3 className={classes.myNickName}>{props.senderName}</h3>
           <p className={classes.MessageBody}>{props.body}</p>
           </div>
+          {props.authUserPhoto?          
           <img  style={{height:"4em",width:"4em"}}
             src={props.authUserPhoto}
             alt="zxc"
+          />
+          :
+          <img  style={{height:"4em",width:"4em"}}
+            src={'https://bain.design/wp-content/uploads/2014/08/People-Avatar-Set-Rectangular-12.jpg'}
+            alt="zxc"
+          }
           />
         </div>
       ) : (
